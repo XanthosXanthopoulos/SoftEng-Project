a85958f87264bfc593ce96e2042e8e17caba00a0
Vassilis <bzafiris@gmail.com>
2020-04-10 20:16:47 +0300

    Renamed to team6 app

--- android/app/src/androidTest/java/com/example/quickrepair/ExampleInstrumentedTest.java
+++ android/app/src/androidTest/java/com/example/quickrepair/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.quickrepair;
 
 import android.content.Context;
 
--- android/app/src/test/java/com/example/quickrepair/ExampleUnitTest.java
+++ android/app/src/test/java/com/example/quickrepair/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.quickrepair;
 
 import org.junit.Test;
 
