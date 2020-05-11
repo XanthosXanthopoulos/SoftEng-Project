b141ae3d10be75031898f5a1fdded62b8a3aacf4
XanthosXanthopoulos <p3170125@aueb.gr>
2020-05-12 01:59:50 +0300

    Added prototype activities

---
+++ android/app/src/main/java/com/example/quickrepair/view/Customer/RegisterCustomer/CustomerRegisterActivity.java
@@ -0,0 +1,18 @@
+package com.example.quickrepair.view.Customer.RegisterCustomer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quickrepair.R;
+
+public class CustomerRegisterActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_register2);
+    }
+}
