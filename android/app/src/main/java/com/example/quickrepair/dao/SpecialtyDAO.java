5155f3bdeaa608fe2546c8013f29e18523623ffb
xrisaD <xrdikonimaki@hotmail.com>
2020-05-11 14:18:53 +0300

    SpecialtyDAO interface

---
+++ android/app/src/main/java/com/example/quickrepair/dao/SpecialtyDAO.java
@@ -0,0 +1,33 @@
+package com.example.quickrepair.dao;
+
+
+import com.example.quickrepair.domain.Specialty;
+
+import java.util.List;
+
+public interface SpecialtyDAO {
+    /**
+     * Find all Specialties
+     * @return Specialties
+     */
+    List<Specialty> findAll();
+
+    /**
+     * Save a Specialty
+     * @param entity Specialty
+     */
+    void save(Specialty entity);
+
+    /**
+     * Find the Specialty with the given id
+     * @param specialtyID Specialty's id
+     * @return Specialty or null
+     */
+    Specialty find(int specialtyID);
+
+    /**
+     * Return the next id for a Specialty.
+     * @return Specialty's id
+     */
+    int nextId();
+}
