71d4fbc066b9b973167331dc9e75578deaf6659c
XanthosXanthopoulos <p3170125@aueb.gr>
2020-05-17 16:53:57 +0300

    Added prototype activities

---
+++ android/app/src/main/java/com/example/quickrepair/view/Technician/AddEditJob.java
@@ -0,0 +1,18 @@
+package com.example.quickrepair.view.Technician;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quickrepair.R;
+
+public class AddEditJob extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit_job);
+    }
+}
